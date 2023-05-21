4fa209b3ea79b57b4efffcae6f3bc5a1951c282f
Swapnils06 <swapnil_k_singh@mckinsey.com>
2023-05-21 23:37:43 +0530

    chore: refactored code base

---
+++ src/constant/routes.js
@@ -0,0 +1,5 @@
+export const HOME_ROUTE = '/';
+export const LOGIN_ROUTE = '/login';
+export const REGISTER_ROUTE = '/register';
+export const ERROR_ROUTE = '/error';
+export const NOT_FOUND_ROUTE = '*';
